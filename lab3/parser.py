e8c27d7c637b8d1a6589db5309f9081701377e6a
platosha-git <15platosha@gmail.com>
2023-06-15 00:09:54 +0300

    done parser

--- lab3/parser.py
+++ lab3/parser.py
@@ -20,168 +20,157 @@ class Parser():
                                       'ol': 0, 'b': 0, 'p': 0}
                 self.parse()
 
-        def _get_name(self, name, op, val = None):
-            new_name = name + str(self.count[name])
-            self.count[name] += 1
-            if op is not None:
-                new_name += ' op: ' + op['value']
-            if val is not None and type(val) != Node:
-                new_name += 'v:' + val['value']
-            return new_name
-
-        def factor(self):
-            lex = self.tokenized_expression.next()
-            if lex is not None and lex['type'] == 'OP_closebrackets':
-                raise CompileError()
-            item = lex
-            is_leaf = True
-            if lex is not None and lex['type'] == 'OP_openbrackets':
-                is_leaf = False
-                item = self.arithmetic_expression()
-                lex = self.tokenized_expression.next()
-                # print('s', self.tokenized_expression.num)
-                if lex is None or lex['type'] != 'OP_closebrackets':
-                    raise CompileError()
-            res = None
-            if item is not None:
-                res = Node(None, [item, None], is_leaf = is_leaf,
-                           node_name = self._get_name('f', None, item))
-            return res
-
-        def term(self):
-            item1 = self.factor()
-            if item1 is None:
-                raise CompileError()
-            item2 = None
-            op = None
-            lex = self.tokenized_expression.next()
-            if lex is not None:
-                while lex is not None and lex['type'] == 'OP_mullike':
-                    if item2 is not None:
-                        item1 = Node(op, [item1, item2],
-                                    node_name = self._get_name('t', op))
-                    op = lex
-                    item2 = self.factor()
-                    if item2 is None:
+        def token_check(self, token, expected_type):
+                if token is None or token['type'] != expected_type:
                         raise CompileError()
-
-                    lex = self.tokenized_expression.next()
-                else:
-                    self.tokenized_expression.prev()
-
-            res = Node(op, [item1, item2],
-                        node_name = self._get_name('t', op))
-            return res
-
-
-        def arithmetic_expression(self):
-            item1 = self.term()
-            if item1 is None:
-                raise CompileError()
-            item2 = None
-            op = None
-            lex = self.tokenized_expression.next()
-            if lex is not None:
-                while lex is not None and lex['type'] == 'OP_addlike':
-                    if item2 is not None:
-                        item1 = Node(op, [item1, item2], node_name = self._get_name('ae', op))
-                    op = lex
-                    item2 = self.term()
-                    if item2 is None:
-                        raise CompileError()
-
-                    lex = self.tokenized_expression.next()
-                else:
-                    self.tokenized_expression.prev()
-            return Node(op, [item1, item2], node_name = self._get_name('ae', op))
-
-        def expression(self, ):
-            item1 = self.arithmetic_expression()
-            item2 = None
-            if item1 is None:
-                raise CompileError()
-            op = None
-            # print(self.tokenized_expression.num)
-            # print(len(self.tokenized_expression.tokens))
-            lex = self.tokenized_expression.next()
-            if lex is not None:
-                if lex['type'] == 'OP_comparelike':
-                    op = lex
-                    item2 = self.arithmetic_expression()
-                    if item2 is None:
-                        raise CompileError()
-                elif lex['type'] not in self.operations_after_expression:
-                    # print(lex)
-                    raise CompileError()
-                else:
-                    self.tokenized_expression.prev()
-
-            return Node(op, [item1, item2], node_name = self._get_name('e', op))
-
-
-        def operator(self):
-            lex = self.tokenized_expression.next()
-            if lex is None or lex['type'] != 'NAME':
-                raise CompileError()
-            item1 = Node(None, [lex, None], is_leaf = True, node_name = self._get_name('i', None, lex))
-            lex = self.tokenized_expression.next()
-            if lex is None or lex['type'] != 'OP_assignment':
-                raise CompileError()
-
-            op = lex
-            item2 = self.expression()
-
-            if item2 is None:
-                raise CompileError()
-
-            return Node(op, [item1, item2], node_name = self._get_name('o', op))
-
-        def operator_list_tail(self):
-            items = []
-            lex = self.tokenized_expression.next()
-            if lex is None or lex['type'] != 'OP_sep':
-                self.tokenized_expression.prev()
-                return None
-            item = self.operator()
-            if item is None:
-                raise CompileError()
-
-
-
-            items = [item]
-            item = self.operator_list_tail()
-            if item is not None:
-                items += item.nodes
-
-            return Node(None, items, node_name = self._get_name('olt', None))
-
-        def operator_list(self):
-            item = self.operator()
-            items = [item]
-            item = self.operator_list_tail()
-            if item is not None:
-                items += item.nodes
-            return Node(None, items, node_name = self._get_name('ol', None))
-
-        def block(self):
-            lex = self.tokenized_expression.next()
-            if lex is None or lex['type'] != 'OP_blockopenbrackets':
-                raise CompileError()
-            item = self.operator_list()
-            lex = self.tokenized_expression.next()
-
-            if lex is None or lex['type'] != 'OP_blockclosebrackets':
-                raise CompileError()
-
-            return Node(None, [item, None], node_name = self._get_name('b', None))
 
         def parse(self):
-                self.programm()
-                if self.tokenized_expression.num < len(self.tokenized_expression.tokens):
-                    raise CompileError()
+                item = self.block()
+                self.root.operations = None
+                self.root.nodes = [item, None]
+                self.root.node_name = self._get_name('p', None)
 
-        def programm(self):
-            item = self.block()
-            self.root.operations = None
-            self.root.nodes = [item, None]
-            self.root.node_name = self._get_name('p', None)
+                if self.tokenized_expression.num < len(self.tokenized_expression.tokens):
+                        raise CompileError()
+
+        def block(self):
+                token = self.tokenized_expression.next()
+                self.token_check(token, 'OP_blockopenbrackets')
+
+                item = self.operator_list()
+
+                token = self.tokenized_expression.next()
+                self.token_check(token, 'OP_blockclosebrackets')
+
+                return Node(None, [item, None], node_name = self._get_name('b', None))
+
+        def operator_list(self):
+                item = self.operator()
+                items = [item]
+                item = self.operator_list_tail()
+                if item is not None:
+                        items += item.nodes
+                return Node(None, items, node_name = self._get_name('ol', None))
+
+        def operator_list_tail(self):
+                token = self.tokenized_expression.next()
+                if token is None or token['type'] != 'OP_sep':
+                        self.tokenized_expression.prev()
+                        return None
+
+                item = self.operator()
+                if item is None:
+                        raise CompileError()
+
+                items = [item]
+                item = self.operator_list_tail()
+                if item is not None:
+                        items += item.nodes
+
+                return Node(None, items, node_name = self._get_name('olt', None))
+
+        def operator(self):
+                token = self.tokenized_expression.next()
+                self.token_check(token, 'NAME')
+
+                item1 = Node(None, [token, None], is_leaf = True, node_name = self._get_name('i', None, token))
+
+                token = self.tokenized_expression.next()
+                self.token_check(token, 'OP_assignment')
+
+                op = token
+                item2 = self.expression()
+                if item2 is None:
+                        raise CompileError()
+
+                return Node(op, [item1, item2], node_name = self._get_name('o', op))
+
+        def expression(self, ):
+                item1 = self.arithmetic_expression()
+                if item1 is None:
+                        raise CompileError()
+
+                item2 = None
+                op = None
+                token = self.tokenized_expression.next()
+                if token is not None:
+                        if token['type'] == 'OP_comparelike':
+                                op = token
+                                item2 = self.arithmetic_expression()
+                                if item2 is None:
+                                    raise CompileError()
+                        elif token['type'] not in self.operations_after_expression:
+                                raise CompileError()
+                        else:
+                                self.tokenized_expression.prev()
+                return Node(op, [item1, item2], node_name = self._get_name('e', op))
+
+        def arithmetic_expression(self):
+                item1 = self.term()
+                if item1 is None:
+                        raise CompileError()
+
+                item2 = None
+                op = None
+                token = self.tokenized_expression.next()
+                if token is not None:
+                        while token is not None and token['type'] == 'OP_addlike':
+                                if item2 is not None:
+                                        item1 = Node(op, [item1, item2], node_name = self._get_name('ae', op))
+                                op = token
+                                item2 = self.term()
+                                if item2 is None:
+                                        raise CompileError()
+
+                                token = self.tokenized_expression.next()
+                        else:
+                                self.tokenized_expression.prev()
+                return Node(op, [item1, item2], node_name = self._get_name('ae', op))
+
+        def term(self):
+                item1 = self.factor()
+                if item1 is None:
+                        raise CompileError()
+
+                item2 = None
+                op = None
+                token = self.tokenized_expression.next()
+                if token is not None:
+                        while token is not None and token['type'] == 'OP_mullike':
+                                if item2 is not None:
+                                        item1 = Node(op, [item1, item2], node_name = self._get_name('t', op))
+                                op = token
+                                item2 = self.factor()
+                                if item2 is None:
+                                        raise CompileError()
+
+                                token = self.tokenized_expression.next()
+                        else:
+                                self.tokenized_expression.prev()
+                return Node(op, [item1, item2], node_name = self._get_name('t', op))
+
+        def factor(self):
+                token = self.tokenized_expression.next()
+                if token is not None and token['type'] == 'OP_closebrackets':
+                        raise CompileError()
+
+                item = token
+                is_leaf = True
+                if token is not None and token['type'] == 'OP_openbrackets':
+                        is_leaf = False
+                        item = self.arithmetic_expression()
+                        token = self.tokenized_expression.next()
+                        if token is None or token['type'] != 'OP_closebrackets':
+                                raise CompileError()
+                if item is not None:
+                        return Node(None, [item, None], is_leaf = is_leaf, node_name = self._get_name('f', None, item))
+                return None
+
+        def _get_name(self, name, op, val = None):
+                new_name = name + str(self.count[name])
+                self.count[name] += 1
+                if op is not None:
+                        new_name += ' op: ' + op['value']
+                if val is not None and type(val) != Node:
+                        new_name += 'v:' + val['value']
+                return new_name
